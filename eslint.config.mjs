241fd19036e3220bc1e38e63f9184e0d27b0bb3a
AshenCinders <albin@edeby.net>
2024-06-02 18:48:02 +0200

    fix: eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            "no-empty": "warn",
+            "no-constant-condition": "warn",
+        },
+    },
+    {
+        ignores: ["jest.config.js", "tests/*", "**/.*"],
+    }
+);
