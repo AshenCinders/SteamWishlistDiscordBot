4dd71bc2fb9d871a3375dac438947bbb52667234
AshenCinders <albin@edeby.net>
2024-06-06 12:11:44 +0200

    feat: wishlist array types

---
+++ src/constructWishlist.ts
@@ -0,0 +1,14 @@
+export type WLGameRec = {
+    appid: string;
+    priority: number;
+    name: string;
+    tags: Array<string>;
+    reviewGrade: string;
+    // Need to convert string to number (is unix time),
+    // value comes as a string from fetch data.
+    releaseDateUnix: number;
+    releaseDateFormatted: string;
+    addedToWLUnix: number;
+};
+
+export type WhitelistArr = Array<WLGameRec>;
