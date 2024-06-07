024ecfb91c719488960b01fd09483242ef031ec5
AshenCinders <albin@edeby.net>
2024-06-07 21:50:59 +0200

    feat: bot interaction handler

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,10 @@
 // Bot startup sequence courtesy of The Coding Train.
 // https://www.youtube.com/@TheCodingTrain
 
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { Client, Events, GatewayIntentBits, Interaction } from 'discord.js';
 import { config } from 'dotenv';
+import * as ping from './commands/ping';
+import * as wishlist from './commands/ping';
 
 config();
 
@@ -15,3 +17,22 @@ client.once(Events.ClientReady, () =>
 );
 
 client.login(process.env.TOKEN);
+
+type recordWithCommands = Record<string, Function>;
+const commandRec: recordWithCommands = {
+    ping: ping.execute,
+    wishlist: wishlist.execute,
+};
+
+async function handleInteraction(interaction: Interaction) {
+    if (!interaction.isCommand()) return;
+    else {
+        await commandRec[interaction.commandName as keyof recordWithCommands](
+            interaction
+        );
+        console.log('Command used: ', interaction.commandName);
+    }
+}
+
+// When user runs command.
+client.on(Events.InteractionCreate, handleInteraction);
