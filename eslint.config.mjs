4a8f2e8d0833ce31f4ac98375d2fcb276193174d
AshenCinders <albin@edeby.net>
2024-06-07 21:58:02 +0200

    eslint: ignore command deploy file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
     {
         rules: {
-            "no-unused-vars": "off",
-            "@typescript-eslint/no-unused-vars": "off",
-            "no-empty": "warn",
-            "no-constant-condition": "warn",
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+            'no-empty': 'warn',
+            'no-constant-condition': 'warn',
         },
     },
     {
-        ignores: ["jest.config.js", "tests/*", "**/.*"],
+        ignores: ['deploy-commands.ts, jest.config.js', 'tests/*', '**/.*'],
     }
 );
