c02f39f621a9054a0a67bc529ebea6a0a135f437
AshenCinders <albin@edeby.net>
2024-06-11 17:50:33 +0200

    feat: get wishlist from id string fn

--- src/wishlists/mainWishlist.ts
+++ src/wishlists/mainWishlist.ts
@@ -1,3 +1,8 @@
+import { Wishlist, constructWishlist } from './constructWishlist';
+import { newWishlistRecord } from './getWishlistData';
+
+export type MaybeWishlist = [true, Wishlist] | [false, string];
+
 /**
  * Rough check to avoid parsing long/empty strings.
  * @param str to be checked.
@@ -13,3 +18,22 @@ function isValidString(str: any): str is string {
             : true;
 }
 
+/**
+ * Generates new user wishlist data from Steam.
+ * @param userIdentifier is the discordID or steam64/custom name identifier
+ *  for a user.
+ * @returns A tuple of a boolean denoting outcome,
+ *  and either a Wishlist,
+ *  or a string explaining what caused the fetch to fail.
+ */
+export function getNewWishlistData(userIdentifier: string): MaybeWishlist {
+    const inputString = userIdentifier.toString();
+    if (!isValidString(inputString))
+        return [false, "The input you've written is invalid."];
+
+    const fetchTuple = newWishlistRecord(inputString);
+    if (fetchTuple[0] === false) return fetchTuple;
+
+    const createdWL: Wishlist = constructWishlist(fetchTuple[1]);
+    return [true, createdWL];
+}
