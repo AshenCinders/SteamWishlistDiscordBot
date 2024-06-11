1f82c32ca0bfaad22e3988c9d13aa2ed39c46f66
AshenCinders <albin@edeby.net>
2024-06-11 17:44:15 +0200

    feat: start of wishlist command

---
+++ src/commands/wishlist.ts
@@ -0,0 +1,58 @@
+import {
+    SlashCommandBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    CommandInteraction,
+    Interaction,
+} from 'discord.js';
+
+export const data = new SlashCommandBuilder()
+    .setName('wishlist')
+    .setDescription('View and share your Steam wishlist through Discord');
+
+/**
+ * Start sequence for wishlist command.
+ * Prompts user with buttons asking if they want to view their or someone else's wishlist. Upon clicking, a new interaction is fired, and this function terminates.
+ * @param interaction slash command 'wishlist'.
+ */
+export async function execute(interaction: CommandInteraction) {
+    const buttonMe = new ButtonBuilder()
+        .setCustomId('myWL')
+        .setLabel('Mine')
+        .setStyle(ButtonStyle.Primary);
+    const buttonElse = new ButtonBuilder()
+        .setCustomId('elseWL')
+        .setLabel("Someone else's")
+        .setStyle(ButtonStyle.Primary);
+
+    const response = await interaction.reply({
+        content: 'Whose wishlist do you wish to see?',
+        ephemeral: true,
+        components: [
+            new ActionRowBuilder<ButtonBuilder>().addComponents(
+                buttonMe,
+                buttonElse
+            ),
+        ],
+    });
+
+    const collectorFilter = (i: Interaction) =>
+        i.user.id === interaction.user.id;
+
+    try {
+        const userChoice = await response.awaitMessageComponent({
+            filter: collectorFilter,
+            time: 60_000,
+        });
+
+        // Once user clicks button, remove this interaction message.
+        await interaction.deleteReply();
+    } catch (err) {
+        console.log(err);
+        await interaction.editReply({
+            content: 'User failed to select an option, cancelling command.',
+            components: [],
+        });
+    }
+}
