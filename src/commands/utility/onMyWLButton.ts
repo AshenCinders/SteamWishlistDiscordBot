4ba3e253ce910f5f0b65b90158cb01e3147658b4
AshenCinders <albin@edeby.net>
2024-06-20 01:16:28 +0200

    feat: working myWishlist button
    yet to do refetch logic

--- src/commands/utility/onMyWLButton.ts
+++ src/commands/utility/onMyWLButton.ts
@@ -1,18 +1,17 @@
 import {
     Interaction,
-    SlashCommandBuilder,
     ButtonInteraction,
-    ModalBuilder,
-    TextInputBuilder,
-    TextInputStyle,
-    ActionRowBuilder,
     ModalSubmitInteraction,
 } from 'discord.js';
+import { newWishlistModal, newRefetchButton } from './constructParts';
 import {
-    fetchWLFromDB,
+    isEligibleToRefetch,
     getNewWishlistData,
     wlToMarkdownCustom,
 } from '../../wishlists/mainWishlist';
+import { unixNow } from '../../miscHelpers';
+import { dbGetWishlist, dbUpdateWishlist } from '../../database/mainDB';
+import { wishlistDBType } from '../../database/dbTypes';
 
 /**
  * Interaction to view 'My wishlist'. If the user is not in DB, then they are prompted to write their identifier into a modal.
@@ -20,75 +19,94 @@ import {
  * @param interaction on pressing 'mywl' button
  */
 export async function execute(interaction: ButtonInteraction) {
-    console.log('Button pressed: mywl');
-    const fetchTuple = fetchWLFromDB(interaction.user.id);
+    const nameOfUser = interaction.user.displayName;
 
-    // DB fetch attempt failed.
-    //fetchTuple[0] === false
-    if (true) {
-        // Prompt user for id
-        console.log('User was not found in DB');
+    const fetchTuple = await dbGetWishlist(interaction.user.id);
+    if (fetchTuple[0] === false) {
+        // DB fetch attempt failed. Prompt user for id
         console.log(fetchTuple[1]);
 
-        const identifierModal = new ModalBuilder({
-            customId: 'idModal',
-            title: 'WishlistBot',
-        });
-        const indentifierInput = new TextInputBuilder({
-            customId: 'idText',
-            label: 'Steam64/custom name',
-            style: TextInputStyle.Paragraph,
-            maxLength: 100,
-            placeholder:
-                'Please write your Steam64ID or custom name (from your custom Steam URL if you have one)',
-            required: true,
-        });
-        const textActionRow =
-            new ActionRowBuilder<TextInputBuilder>().addComponents(
-                indentifierInput
-            );
-        identifierModal.addComponents(textActionRow);
-
-        await interaction.showModal(identifierModal);
-
-        const filter = (interaction: ModalSubmitInteraction) =>
-            interaction.customId === 'idModal';
-
-        await interaction
-            .awaitModalSubmit({ filter, time: 300_000 })
-            // ASYNC IMPORTANT!
-            .then(async (modalInteraction) => {
-                const textInput =
-                    modalInteraction.fields.getTextInputValue('idText');
-                console.log(textInput);
-
-                await modalInteraction.deferReply({
-                    ephemeral: true,
-                });
-
-                // start sequence and save to DB
-                // TODO
-                getNewWishlistData(textInput).then(async (wlTuple) => {
-                    if (wlTuple[0] === false) {
-                        console.log(wlTuple[1]);
-                        await modalInteraction.editReply({
-                            content: wlTuple[1],
-                        });
-                    } else {
-                        const displayStr = wlToMarkdownCustom(wlTuple[1], {});
-                        await modalInteraction.editReply({
-                            content: displayStr,
-                        });
-                    }
-                });
-            })
-            .catch((err) => {
-                console.log(err);
+        const wlModal = newWishlistModal();
+        try {
+            await interaction.showModal(wlModal);
+            const filter = (interaction: ModalSubmitInteraction) =>
+                interaction.customId === 'idModal';
+            const modalInteraction = await interaction.awaitModalSubmit({
+                filter,
+                time: 600_000,
             });
+            await modalInteraction.deferReply({
+                ephemeral: true,
+            });
+
+            const textInput =
+                modalInteraction.fields.getTextInputValue('idText');
+            console.log(
+                `User ${nameOfUser} inputted steam identifier: ${textInput}`
+            );
+
+            const wlTuple = await getNewWishlistData(textInput);
+            if (wlTuple[0] === false) {
+                console.log(
+                    `Steam failed to find a wishlist from user ${nameOfUser}'s ID input`
+                );
+                await modalInteraction.editReply({
+                    content: wlTuple[1],
+                });
+            } else {
+                const displayStr = wlToMarkdownCustom(wlTuple[1], {});
+                console.log(`Displayed a wishlist to user ${nameOfUser}`);
+                await modalInteraction.editReply({
+                    content: displayStr,
+                });
+
+                // Store to DB
+                const dbWishlistData: wishlistDBType = {
+                    givenIdentifier: textInput,
+                    discordIdentifier: interaction.user.id,
+                    unixFetchedAt: unixNow(),
+                    wishlistData: wlTuple[1],
+                };
+                await dbUpdateWishlist(dbWishlistData);
+            }
+        } catch (err) {
+            console.log(err);
+            return;
+        }
+    } else {
         // DB fetch attempt succeeded
-    } else console.log('in db');
-    /* fetchTuple[1] is data
-        load and show how long since fetch.
-        Let user click button to refresh it, be unclickable 
-        if within ~10min since. */
+        console.log(`${nameOfUser}'s wishlist was found in DB`);
+
+        const wishlist = fetchTuple[1].wishlistData;
+        const displayStr = wlToMarkdownCustom(wishlist, {});
+
+        let disableButton = isEligibleToRefetch(fetchTuple[1].unixFetchedAt)
+            ? false
+            : true;
+        const refetchButton = newRefetchButton(
+            disableButton,
+            'Update data (after 10min)'
+        );
+
+        console.log(`Displayed a wishlist to user ${nameOfUser}`);
+        const response = await interaction.reply({
+            content: displayStr,
+            ephemeral: true,
+            components: [refetchButton],
+        });
+
+        // Handle interaction in another file through bot event handler.
+        const collectorFilter = (i: Interaction) =>
+            i.user.id === interaction.user.id;
+        try {
+            await response.awaitMessageComponent({
+                filter: collectorFilter,
+                time: 60_000,
+            });
+            // Once user clicks button, remove this interaction message.
+            await interaction.deleteReply();
+        } catch (err) {
+            console.log('Option to refetch wishlist data timed out');
+        }
+    }
 }
