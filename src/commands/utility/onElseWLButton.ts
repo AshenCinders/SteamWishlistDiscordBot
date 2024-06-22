8d7badb4782b73f13328e597855f3c6304a70fab
AshenCinders <albin@edeby.net>
2024-06-22 21:06:33 +0200

    feat: view someone else's WL

--- src/commands/utility/onElseWLButton.ts
+++ src/commands/utility/onElseWLButton.ts
@@ -1,13 +1,9 @@
+import { ButtonInteraction, ModalSubmitInteraction } from 'discord.js';
+import { newWishlistModal } from './constructParts';
 import {
-    Interaction,
-    SlashCommandBuilder,
-    ButtonInteraction,
-    ModalBuilder,
-    ModalSubmitInteraction,
-    TextInputBuilder,
-    TextInputStyle,
-    ActionRowBuilder,
-} from 'discord.js';
+    getNewWishlistData,
+    wlToMarkdownCustom,
+} from '../../wishlists/mainWishlist';
 
 /**
  * Interaction to view 'Someone else's wishlist'. The user gets prompted to input the identifier for the account's wishlist they wish to see.
@@ -15,49 +11,41 @@ import {
  * @param interaction on pressing 'elsewl' button
  */
 export async function execute(interaction: ButtonInteraction) {
-    console.log('Button pressed: elsewl');
-
-    const identifierModal = new ModalBuilder({
-        customId: 'idModal',
-        title: 'WishlistBot',
-    });
-    const indentifierInput = new TextInputBuilder({
-        customId: 'idText',
-        label: 'Steam64/custom name',
-        style: TextInputStyle.Paragraph,
-        maxLength: 100,
-        placeholder:
-            'Please write the Steam64ID or custom name (from the custom Steam URL if there is one)',
-        required: true,
-    });
-    const textActionRow =
-        new ActionRowBuilder<TextInputBuilder>().addComponents(
-            indentifierInput
-        );
-    identifierModal.addComponents(textActionRow);
-
-    await interaction.showModal(identifierModal);
-
-    const filter = (interaction: ModalSubmitInteraction) =>
-        interaction.customId === 'idModal';
-
-    await interaction
-        .awaitModalSubmit({ filter, time: 300_000 })
-        // ASYNC IMPORTANT!
-        .then(async (modalInteraction) => {
-            const textInput =
-                modalInteraction.fields.getTextInputValue('idText');
-            console.log(textInput);
-
-            await modalInteraction.deferReply({
-                ephemeral: true,
-            });
-
-            // start sequence and DON'T save user WL
-            // TODO
-            await modalInteraction.editReply({ content: 'data gotten' });
-        })
-        .catch((err) => {
-            console.log(err);
+    const nameOfUser = interaction.user.displayName;
+    const wlModal = newWishlistModal();
+    try {
+        await interaction.showModal(wlModal);
+        const filter = (interaction: ModalSubmitInteraction) =>
+            interaction.customId === 'idModal';
+        const modalInteraction = await interaction.awaitModalSubmit({
+            filter,
+            time: 600_000,
         });
+        await modalInteraction.deferReply({
+            ephemeral: true,
+        });
+
+        const textInput = modalInteraction.fields.getTextInputValue('idText');
+        console.log(
+            `User ${nameOfUser} inputted steam identifier: ${textInput}`
+        );
+
+        const wlTuple = await getNewWishlistData(textInput);
+        if (wlTuple[0] === false) {
+            console.log(
+                `Steam failed to find a wishlist from user ${nameOfUser}'s ID input`
+            );
+            await modalInteraction.editReply({
+                content: wlTuple[1],
+            });
+        } else {
+            const displayStr = wlToMarkdownCustom(wlTuple[1], {});
+            console.log(`Displayed a wishlist to user ${nameOfUser}`);
+            await modalInteraction.editReply({
+                content: displayStr,
+            });
+        }
+    } catch (err) {
+        console.log(err);
+    }
 }
