798375ce2fd52fef0d3dc14a4d53a105bc320206
AshenCinders <albin@edeby.net>
2024-06-23 23:34:21 +0200

    test: mock wishlist data

---
+++ tests/mockData/rawWishlistData1.ts
@@ -0,0 +1,256 @@
+export const rawWishlistData1 = {
+    '526870': {
+        'name': 'Satisfactory',
+        'capsule':
+            'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/526870/header_292x136.jpg?t=1718088881',
+        'review_score': 9,
+        'review_desc': 'Overwhelmingly Positive',
+        'reviews_total': '138,736',
+        'reviews_percent': 97,
+        'release_date': '1591635600',
+        'release_string': '8 Jun, 2020',
+        'platform_icons':
+            '\u003Cspan class="platform_img win"\u003E\u003C/span\u003E',
+        'subs': [
+            {
+                'packageid': 126218,
+                'bundleid': null,
+                'discount_block':
+                    '\u003Cdiv class="discount_block discount_block_large" data-price-final="1499" data-bundlediscount="0" data-discount="50" role="link" aria-label="50% off. 29,99€ normally, discounted to 14,99€"\u003E\u003Cdiv class="discount_pct"\u003E-50%\u003C/div\u003E\u003Cdiv class="discount_prices"\u003E\u003Cdiv class="discount_original_price"\u003E29,99€\u003C/div\u003E\u003Cdiv class="discount_final_price"\u003E14,99€\u003C/div\u003E\u003C/div\u003E\u003C/div\u003E',
+                'discount_pct': 50,
+                'price': '1499',
+            },
+        ],
+        'type': 'Game',
+        'screenshots': [
+            'ss_9083ccf8fbb22c4068a6e233e3de343f815f9dc1.jpg',
+            'ss_324fb3d8774ef3eadba136d4eae8c01cbdf14a83.jpg',
+            'ss_dcb79211f2f9bac1e6a2d85aad132fc3ae909001.jpg',
+            'ss_cff8791f06839134d09f4c865758193a7023b92f.jpg',
+        ],
+        'review_css': 'positive',
+        'priority': 0,
+        'added': 1712929535,
+        'background':
+            'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/526870/page_bg_generated_v6b.jpg?t=1718088881',
+        'rank': 482,
+        'tags': [
+            'Base Building',
+            'Automation',
+            'Open World',
+            'Multiplayer',
+            'Crafting',
+        ],
+        'is_free_game': false,
+        'deck_compat': '2',
+        'early_access': 1,
+        'win': 1,
+    },
+    '1210320': {
+        'name': 'Potion Craft: Alchemist Simulator',
+        'capsule':
+            'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1210320/header_292x136.jpg?t=1710524732',
+        'review_score': 8,
+        'review_desc': 'Very Positive',
+        'reviews_total': '25,583',
+        'reviews_percent': 92,
+        'release_date': '1670950811',
+        'release_string': '13 Dec, 2022',
+        'platform_icons':
+            '\u003Cspan class="platform_img win"\u003E\u003C/span\u003E',
+        'subs': [
+            {
+                'packageid': 418303,
+                'bundleid': null,
+                'discount_block':
+                    '\u003Cdiv class="discount_block discount_block_large no_discount" data-price-final="1950" data-bundlediscount="0" data-discount="0"\u003E\u003Cdiv class="discount_prices"\u003E\u003Cdiv class="discount_final_price"\u003E19,50€\u003C/div\u003E\u003C/div\u003E\u003C/div\u003E',
+                'discount_pct': null,
+                'price': '1950',
+            },
+        ],
+        'type': 'Game',
+        'screenshots': [
+            'ss_4cd75f60776b9e4cf6170f01da26087811d77d81.jpg',
+            'ss_edaacdffd002753210063862b7af7096f8de098b.jpg',
+            'ss_bc7f1f1182b80be859173cd5313c546ae1aede94.jpg',
+            'ss_536d1f0e7c8b69de3f69bc1ef76dad503b639f76.jpg',
+        ],
+        'review_css': 'positive',
+        'priority': 8,
+        'added': 1703182343,
+        'background':
+            'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/526870/page_bg_generated_v6b.jpg?t=1718088881',
+        'rank': 816,
+        'tags': [
+            'Crafting',
+            'Singleplayer',
+            'Relaxing',
+            'Simulation',
+            'Medieval',
+        ],
+        'is_free_game': false,
+        'deck_compat': '3',
+        'win': 1,
+    },
+    '1599600': {
+        'name': 'PlateUp!',
+        'capsule':
+            'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1599600/header_292x136.jpg?t=1716562055',
+        'review_score': 9,
+        'review_desc': 'Overwhelmingly Positive',
+        'reviews_total': '13,001',
+        'reviews_percent': 96,
+        'release_date': '1659621694',
+        'release_string': '4 Aug, 2022',
+        'platform_icons':
+            '\u003Cspan class="platform_img win"\u003E\u003C/span\u003E',
+        'subs': [
+            {
+                'packageid': 566472,
+                'bundleid': null,
+                'discount_block':
+                    '\u003Cdiv class="discount_block discount_block_large no_discount" data-price-final="1950" data-bundlediscount="0" data-discount="0"\u003E\u003Cdiv class="discount_prices"\u003E\u003Cdiv class="discount_final_price"\u003E19,50€\u003C/div\u003E\u003C/div\u003E\u003C/div\u003E',
+                'discount_pct': null,
+                'price': '1950',
+            },
+        ],
+        'type': 'Game',
+        'screenshots': [
+            'ss_8c944c593113598b6446b6b5d07962a10fe79ec6.jpg',
+            'ss_2fb45e66d1b4e8c3d8fe6743fad278bcc082c2d5.jpg',
+            'ss_833de2e09f19972bb9e7db021c793da6afdab8a0.jpg',
+            'ss_5cdb7d8147767fece03a9a69a10af64a2e0c5e47.jpg',
+        ],
+        'review_css': 'positive',
+        'priority': 0,
+        'added': 1703269627,
+        'background':
+            'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/526870/page_bg_generated_v6b.jpg?t=1718088881',
+        'rank': 948,
+        'tags': ['Co-op', 'Management', 'Cooking', 'Roguelite', 'Building'],
+        'is_free_game': false,
+        'deck_compat': '3',
+        'win': 1,
+    },
+    '1778820': {
+        'name': 'TEKKEN 8',
+        'capsule':
+            'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1778820/header_292x136.jpg?t=1716796901',
+        'review_score': 6,
+        'review_desc': 'Mostly Positive',
+        'reviews_total': '21,116',
+        'reviews_percent': 70,
+        'release_date': '1706223600',
+        'release_string': '26 Jan, 2024',
+        'platform_icons':
+            '\u003Cspan class="platform_img win"\u003E\u003C/span\u003E',
+        'subs': [
+            {
+                'packageid': 912716,
+                'bundleid': null,
+                'discount_block':
+                    '\u003Cdiv class="discount_block discount_block_large no_discount" data-price-final="6999" data-bundlediscount="0" data-discount="0"\u003E\u003Cdiv class="discount_prices"\u003E\u003Cdiv class="discount_final_price"\u003E69,99€\u003C/div\u003E\u003C/div\u003E\u003C/div\u003E',
+                'discount_pct': null,
+                'price': '6999',
+            },
+        ],
+        'type': 'Game',
+        'screenshots': [
+            'ss_62e6ec252bc1a641e8e42dba07f23631d5da85e6.jpg',
+            'ss_99dabe34abc0f92adc39d7099908c9540be7eb3f.jpg',
+            'ss_bbd38a5fe748ec966cbc85ffcd4931f0f2da8ffd.jpg',
+            'ss_7c55021d3ba8e4f14c6d9dbea9f19d9b7665d5f0.jpg',
+        ],
+        'review_css': 'positive',
+        'priority': 133,
+        'added': 1706285265,
+        'background':
+            'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/526870/page_bg_generated_v6b.jpg?t=1718088881',
+        'rank': 823,
+        'tags': ['Fighting', 'Action', '3D Fighter', 'Multiplayer', 'eSports'],
+        'is_free_game': false,
+        'deck_compat': '2',
+        'win': 1,
+    },
+    '2178590': {
+        'name': 'Coin Pusher Casino',
+        'capsule':
+            'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2178590/header_292x136.jpg?t=1709203897',
+        'review_score': 8,
+        'review_desc': 'Very Positive',
+        'reviews_total': '157',
+        'reviews_percent': 88,
+        'release_date': '1709203522',
+        'release_string': '29 Feb, 2024',
+        'platform_icons':
+            '\u003Cspan class="platform_img win"\u003E\u003C/span\u003E\u003Cspan class="platform_img linux"\u003E\u003C/span\u003E',
+        'subs': [
+            {
+                'packageid': 780036,
+                'bundleid': null,
+                'discount_block':
+                    '\u003Cdiv class="discount_block discount_block_large no_discount" data-price-final="1250" data-bundlediscount="0" data-discount="0"\u003E\u003Cdiv class="discount_prices"\u003E\u003Cdiv class="discount_final_price"\u003E12,50€\u003C/div\u003E\u003C/div\u003E\u003C/div\u003E',
+                'discount_pct': null,
+                'price': '1250',
+            },
+        ],
+        'type': 'Game',
+        'screenshots': [
+            'ss_4edeb325fb6d5348ba46dfce3157b6834c1d2530.jpg',
+            'ss_670d3e6f906d0b8f62fa837443c6a0cec09c7eeb.jpg',
+            'ss_3aed68eb66411a41ff8101fadd83379f994e5af3.jpg',
+            'ss_75bb3f3f74ffff9b269bd72d40f6363187a51ea9.jpg',
+        ],
+        'review_css': 'positive',
+        'priority': 3,
+        'added': 1701393560,
+        'background':
+            'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/526870/page_bg_generated_v6b.jpg?t=1718088881',
+        'rank': 1000,
+        'tags': ['Simulation', 'Casual', 'Arcade', 'Gambling', 'Loot'],
+        'is_free_game': false,
+        'deck_compat': '3',
+        'win': 1,
+        'linux': 1,
+    },
+    '2292800': {
+        'name': 'Blacksmith Master',
+        'capsule':
+            'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2292800/header_292x136.jpg?t=1718716221',
+        'review_score': 0,
+        'review_desc': 'No user reviews',
+        'reviews_total': '0',
+        'reviews_percent': 0,
+        'release_date': 1719138642,
+        'release_string': '2024',
+        'platform_icons':
+            '\u003Cspan class="platform_img win"\u003E\u003C/span\u003E\u003Cspan class="platform_img mac"\u003E\u003C/span\u003E\u003Cspan class="platform_img linux"\u003E\u003C/span\u003E',
+        'subs': [],
+        'type': 'Game',
+        'screenshots': [
+            'ss_68170700b6890f94e08b8a29811d72123c79ce47.jpg',
+            'ss_97da04d9b34b0c51972fe29c64c6a50213d2257b.jpg',
+            'ss_f93ac9096c71b501cfa797d99b7fbe18481481e1.jpg',
+            'ss_b96997920b4d2d3654c30ff90c18c37fb61f23bb.jpg',
+        ],
+        'review_css': 'no_reviews',
+        'priority': 33,
+        'added': 1700790566,
+        'background':
+            'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/526870/page_bg_generated_v6b.jpg?t=1718088881',
+        'rank': 1000,
+        'tags': [
+            'Strategy',
+            'Simulation',
+            'Management',
+            'Base Building',
+            'Sandbox',
+        ],
+        'is_free_game': false,
+        'deck_compat': 0,
+        'prerelease': 1,
+        'win': 1,
+        'mac': 1,
+        'linux': 1,
+    },
+};
