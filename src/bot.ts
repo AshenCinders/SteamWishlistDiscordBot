0e312cef010ae84302279d87dad6caf9497b256b
AshenCinders <albin@edeby.net>
2024-06-28 00:43:53 +0200

    improve: stop bot on db startup fail

--- src/bot.ts
+++ src/bot.ts
@@ -22,7 +22,7 @@ mongoose
     })
     .catch((err: Error) => {
         console.log('MongoDB connection error. ' + err);
-        //process.exit();
+        process.exit();
     });
 
 // Connect to Discord server.
