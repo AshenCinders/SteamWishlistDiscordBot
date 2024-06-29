69e5747eb6bcf1f24aa675ce3d7ded15d88e4d79
AshenCinders <albin@edeby.net>
2024-06-29 19:22:00 +0200

    refactor: elseWLButton

--- src/commands/utility/buttonElseWL.ts
+++ src/commands/utility/buttonElseWL.ts
@@ -12,41 +12,41 @@ import {
  */
 export async function onElseWLButton(interaction: ButtonInteraction) {
     const nameOfUser = interaction.user.displayName;
+
     const wlModal = newWishlistModal();
+    let modalSubmit: ModalSubmitInteraction | undefined;
     try {
         await interaction.showModal(wlModal);
         const filter = (interaction: ModalSubmitInteraction) =>
             interaction.customId === 'idModal';
-        const modalInteraction = await interaction.awaitModalSubmit({
+        modalSubmit = await interaction.awaitModalSubmit({
             filter,
             time: 600_000,
         });
-        await modalInteraction.deferReply({
+        await modalSubmit.deferReply({
             ephemeral: true,
         });
-
-        const textInput = modalInteraction.fields.getTextInputValue('idText');
-        console.log(
-            `User ${nameOfUser} inputted steam identifier: ${textInput}`
-        );
-
-        const wlTuple = await fetchNewWishlist(textInput);
-        if (wlTuple[0] === false) {
-            console.log(
-                `Steam failed to find a wishlist from user ${nameOfUser}'s ID input`
-            );
-            await modalInteraction.editReply({
-                // Display reason why the fetch failed.
-                content: wlTuple[1],
-            });
-        } else {
-            const displayStr = wlToMarkdownCustom(wlTuple[1]);
-            console.log(`Displayed a wishlist to user ${nameOfUser}`);
-            await modalInteraction.editReply({
-                content: displayStr,
-            });
-        }
     } catch (err) {
-        console.log(err);
+        // Submit timed out, stop interaction.
+        console.log("Wishlist modal timed out (someone else's wishlist)");
+        return;
+    }
+
+    const textInput = modalSubmit.fields.getTextInputValue('idText');
+    const wlTuple = await fetchNewWishlist(textInput);
+    if (wlTuple[0] === false) {
+        console.log(
+            `Steam failed to find a wishlist from user ${nameOfUser}'s ID input`
+        );
+        await modalSubmit.editReply({
+            // Display reason why the fetch failed.
+            content: wlTuple[1],
+        });
+    } else {
+        const displayStr = wlToMarkdownCustom(wlTuple[1]);
+        console.log(`Displayed a wishlist to user ${nameOfUser}`);
+        await modalSubmit.editReply({
+            content: displayStr,
+        });
     }
 }
