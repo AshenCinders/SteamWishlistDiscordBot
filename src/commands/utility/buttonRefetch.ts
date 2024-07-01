f9981a5a4933dd1260277971ee70ac9d8e6e8dba
AshenCinders <albin@edeby.net>
2024-07-01 19:26:29 +0200

    refactor: refetch button

--- src/commands/utility/buttonRefetch.ts
+++ src/commands/utility/buttonRefetch.ts
@@ -20,19 +20,17 @@ export async function onRefetchButton(interaction: ButtonInteraction) {
     }
 
     const givenID = fetchTuple[1].givenIdentifier;
-    const wlTuple = await fetchNewWishlist(givenID);
-    if (wlTuple[0] === false) {
+    const wishlistTuple = await fetchNewWishlist(givenID);
+    if (wishlistTuple[0] === false) {
         await interaction.reply({
             content: 'Unable to get data from Steam, try again later.',
         });
         return;
     }
 
-    const displayStr = wlToMarkdownCustom(wlTuple[1]);
-
     console.log(`Displayed a wishlist to user ${interaction.user.displayName}`);
     interaction.editReply({
-        content: displayStr,
+        content: wlToMarkdownCustom(wishlistTuple[1]),
     });
 
     // Store to DB
@@ -40,7 +38,7 @@ export async function onRefetchButton(interaction: ButtonInteraction) {
         givenIdentifier: givenID,
         discordIdentifier: interaction.user.id,
         unixFetchedAt: unixNow(),
-        wishlistData: wlTuple[1],
+        wishlistData: wishlistTuple[1],
     };
     dbUpdateWishlist(dbWishlistData);
 }
